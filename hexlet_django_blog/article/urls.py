57e1006d6a2005297987c92c313bf33ff77c7d92
tarvarrs <125196841+tarvarrs@users.noreply.github.com>
2025-03-16 20:09:28 +0300

    add named url

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -3,4 +3,5 @@ from hexlet_django_blog.article import views
 
 urlpatterns = [
     path('', views.ArticlesHomePageView.as_view()),
+    path('<str:tags>/<int:article_id>/', views.index, name='article'),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views import View
-
+from django.urls import reverse
 
 # Create your views here.
 class ArticlesHomePageView(View):
@@ -11,3 +11,10 @@ class ArticlesHomePageView(View):
         context = super().get_context_data(**kwargs)
         context['app_name'] = 'articles'
         return context
+    
+    def get(self, request):
+        return redirect(reverse('article', kwargs={'tags': 'python', 'article_id': 42}))
+
+
+def index(request, tags, article_id):
+    return HttpResponse(f'Статья номер {article_id}. Тег {tags}')
