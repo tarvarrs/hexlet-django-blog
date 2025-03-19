c628d65c0e4b0197742c0cb82fcc6345243e2cec
tarvarrs <125196841+tarvarrs@users.noreply.github.com>
2025-03-19 11:38:32 +0300

    add article display by id

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from hexlet_django_blog.article.views import IndexView
+from hexlet_django_blog.article.views import IndexView, ArticleView # , ArticleCommentsView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='article'),
+    path('<int:id>/', ArticleView.as_view(), name='articleGet'),
+    # path('<int:article_id>/comments/<int:id>', ArticleCommentsView.as_view()),
     # path('<str:tags>/<int:article_id>/', views.index, name='article'),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.views import View
 from django.urls import reverse
@@ -21,3 +21,16 @@ class IndexView(View):
 
 def index(request, tags, article_id):
     return HttpResponse(f'Статья номер {article_id}. Тег {tags}')
+
+
+class ArticleView(View):
+
+    def get(self, request, *args, **kwargs):
+        article = get_object_or_404(Article, id=kwargs['id'])
+        return render(request, 'articles/show.html', context={'article': article})
+
+
+# class ArticleCommentsView(View):
+
+#     def get(self, request, *args, **kwargs):
+#         comment = get_object_or_404(Comment, id=kwargs['id'], article__id=kwargs['article_id'])
